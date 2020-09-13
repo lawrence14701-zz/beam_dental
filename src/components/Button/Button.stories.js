439f3dabbe84bfa156384bb9fd0180c424c004fd
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2020-09-13 15:39:47 -0400

    add storybook button example

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from './Button'
+
+export default {
+    title: 'cats',
+    component: Button
+}
+
+export const Primary = () => <Button variant='primary'>Primary</Button>
+export const Secondary = () => <Button variant='secondary'>secondary</Button>
